e1fef9948492592accf9b7fded0856ae6152d6b9
Waseeftauqueer <waseeftauqueer.a2022ece@sece.ac.in>
2024-03-11 00:04:48 +0530

    Spot Details Page Completed

--- travelplanner/src/App.js
+++ travelplanner/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import Roadmap from './SpotDetails/Roadmap';
 // import './CardTemplate/Cards'
-import Nav from './navbar/nav';
+import Nav from './navbar/Nav';
 
 import Cards from './CardTemplate/Cards';
 
 function App() {
   return (
     <>
-    <Nav/>
-    <Cards /></>
+      <Roadmap/>
+    </>
   );
 }
 
